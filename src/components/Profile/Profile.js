24dcda5528706332a8ee1bc078bdddc6f81f89f6
Jorgen <ybeliaev@gmail.com>
2021-06-11 13:49:35 +0300

    Profile carcas complite

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 import Paper from '@material-ui/core/Paper'
 import Grid from '@material-ui/core/Grid'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import Divider from '@material-ui/core/Divider'
+import ListItemText from '@material-ui/core/ListItemText'
+import ListItemAvatar from '@material-ui/core/ListItemAvatar'
+import Avatar from '@material-ui/core/Avatar'
+import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles((theme) => ({
@@ -10,13 +17,45 @@ const useStyles = makeStyles((theme) => ({
         textAlign: 'center',
         color: theme.palette.text.secondary,
     },
+    inline: {
+        display: 'inline',
+    },
 }))
 
 export const Profile = () => {
     const classes = useStyles()
     return (
         <Grid item xs={6}>
-            <Paper className={classes.paper}>xs=6</Paper>
+            <Paper className={classes.paper}>
+                <List className={classes.root}>
+                    <ListItem alignItems="flex-start">
+                        <ListItemAvatar>
+                            <Avatar
+                                alt="Remy Sharp"
+                                src="/static/images/avatar/1.jpg"
+                            />
+                        </ListItemAvatar>
+                        <ListItemText
+                            primary="Brunch this weekend?"
+                            secondary={
+                                <React.Fragment>
+                                    <Typography
+                                        component="span"
+                                        variant="body2"
+                                        className={classes.inline}
+                                        color="textPrimary"
+                                    >
+                                        Ali Connors
+                                    </Typography>
+                                    {
+                                        " — I'll be in your neighborhood doing errands this…"
+                                    }
+                                </React.Fragment>
+                            }
+                        />
+                    </ListItem>
+                </List>
+            </Paper>
         </Grid>
     )
 }
