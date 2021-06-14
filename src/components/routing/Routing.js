6d5ee0f95986aec58329ee752aa683e6f6940d96
Jorgen <ybeliaev@gmail.com>
2021-06-14 14:18:47 +0300

    add react-router-dom & dir routing

---
+++ src/components/routing/Routing.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const Routing = () => {
+    return <div></div>
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import { createStore } from 'redux'
-
+import { composeWithDevTools } from 'redux-devtools-extension'
 import { reducers } from './reducers'
 
-export const store = createStore(reducers)
+export const store = createStore(reducers, composeWithDevTools())
