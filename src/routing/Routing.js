c22d62f0a3bcede5d46092d2cfc6c24bd209032f
Jorgen <ybeliaev@gmail.com>
2021-06-14 21:28:27 +0300

    path constants add

--- src/routing/Routing.js
+++ src/routing/Routing.js
@@ -2,18 +2,18 @@ import React from 'react'
 
 import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import { Main, Contacts, About, NotFound } from '../pages/Main'
-
+import { path } from '../static/constants'
 export const Routing = () => {
     return (
         <Router>
             <Switch>
-                <Route path="/" exact>
+                <Route path={path.main} exact>
                     <Main />
                 </Route>
-                <Route path="/contacts" exact>
+                <Route path={path.contacts} exact>
                     <Contacts />
                 </Route>
-                <Route path="/about" exact>
+                <Route path={path.about} exact>
                     <About />
                 </Route>
 
---
+++ src/static/constants/index.js
@@ -0,0 +1,5 @@
+export const path = {
+    main: '/',
+    contacts: '/contacts',
+    about: '/about',
+}
