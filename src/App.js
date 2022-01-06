906f93ef8b171cb77a4044e11b60e0e4298156a8
Yurij Beliaev <ybeliaev@gmail.com>
2022-01-06 13:21:13 +0200

    deleted unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
 import React, { useContext, useState } from 'react'
 import './App.css'
-import PropTypes from 'prop-types'
 
-import Cards from './components/Cards'
-import Modal from './components/Modal'
-
-import storeContext from './contexts/store'
-import { observer } from 'mobx-react-lite'
 
 function App() {
-    let { cart } = useContext(storeContext)
-    console.log({ cart })
-    let { products, remove, onChangeCount, total } = cart
+    
 
     return (
-        <div className="wrapper">
-            <Cards
-                data={products}
-                onChangeCount={onChangeCount}
-                remove={remove}
-            />
-            <div>
-                <span>Total: </span>
-                <strong>{total}</strong>
-            </div>
-            <Modal />
-        </div>
+        <div>react</div>
     )
-}
-App.propTypes = {
-    onChangeCount: PropTypes.func,
 }
 export default App
