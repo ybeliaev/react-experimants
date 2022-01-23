939a37a0c53127349aee7e35173b98d32884cf86
Yurij  Beliaev <ybeliaev@gmail.com>
2022-01-23 14:43:16 +0200

    add handleEdit

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,14 +1,19 @@
 import React from 'react'
-import PropTypes from 'prop-types';
-
+import PropTypes from 'prop-types'
 
 export const Card = ({ name, dispatch }) => {
     const handleDelete = () => {
         dispatch({
-            type: "DELETE_NAME",
-            payload: name
+            type: 'DELETE_NAME',
+            payload: name,
         })
-        console.log("delete name: ", name)
+        console.log('delete name: ', name)
+    }
+    const handleEdit = () => {
+        dispatch({
+            type: 'EDIT_NAME',
+            payload: name,
+        })
     }
     return (
         <div className="card_wrapper card">
@@ -19,13 +24,17 @@ export const Card = ({ name, dispatch }) => {
                 </h4>
             </div>
             <div>
-                <button className="btn">edit</button>
-                <button className="btn btn-alert" onClick={handleDelete}>del</button>
+                <button className="btn" onClick={handleEdit}>
+                    edit
+                </button>
+                <button className="btn btn-alert" onClick={handleDelete}>
+                    del
+                </button>
             </div>
         </div>
     )
 }
 Card.propTypes = {
     name: PropTypes.string,
-    dispatch: PropTypes.func
+    dispatch: PropTypes.func,
 }
